7a47ef7a8c578ef14ce5f756cdef10294ef190a1
Vlad Ataman <bearlikecode@gmail.com>
2025-04-15 16:35:45 +0300

    Added tls support for mongo db

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Загружаем переменные из .env
 
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
 
 db = client["mafia_game"]
 users_collection = db["Users"]
